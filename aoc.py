6c6b7c031d7b60743fdb32955696b00e6a6fd2eb
EDDxample <EDDxample@gmail.com>
2021-04-21 02:03:00 +0200

    add time tracking and faster parts support

--- aoc.py
+++ aoc.py
@@ -10,7 +10,7 @@ def main():
 
 
     except ValueError as error:
-        print('usage: py aoc.py [gen|run] <year> <day>')
+        print(f'{error}\nusage: py aoc.py [gen|run] <year> <day>')
 
 
 
@@ -28,14 +28,24 @@ def run_solutions(year, day):
     x = module.part2(puzzle_input)
     t1 = time.time()
     print(f'part 2: {x} {((t1 - t0) * 1000):.3f}ms')
-    print()
+    try:
+        t0 = time.time()
+        x = module.faster_part1(puzzle_input)
+        t1 = time.time()
+        print(f'faster part 1: {x} {((t1 - t0) * 1000):.3f}ms')
+        t0 = time.time()
+        x = module.faster_part2(puzzle_input)
+        t1 = time.time()
+        print(f'faster part 2: {x} {((t1 - t0) * 1000):.3f}ms')
+        print()
+    except: pass
 
 def gen_day(year, day):
     path = f'aoc/{year}/{day}'
     puzzle_input = get_input(year, day)
     write_file(path+'/input.txt', puzzle_input)
     write_file(path+'/description.txt', f'https://adventofcode.com/{year}/day/{day}')
-    write_file(path+'/solution.py', '\ndef part1(puzzle_in):\n\treturn \'not implemented yet\'\n\ndef part2(puzzle_in):\n\treturn \'not implemented yet\'\n')
+    write_file(path+'/solution.py', '\ndef part1(puzzle_in: str):\n\treturn \'not implemented yet\'\n\ndef part2(puzzle_in: str):\n\treturn \'not implemented yet\'\n\ndef faster_part1(puzzle_in: str):\n\treturn \'not implemented yet\'\n\ndef faster_part2(puzzle_in: str):\n\treturn \'not implemented yet\'\n')
 
 def get_input(year, day):
     with open('.token') as token:
