5d2c0a84314242e935b0310c9fe96db0713950f9
EDDxample <EDDxample@gmail.com>
2021-04-22 02:45:19 +0200

    complete 2015 day 6

---
+++ aoc/2015/6/solution.py
@@ -0,0 +1,41 @@
+import numpy as np, re
+grid  = np.zeros((1000, 1000), dtype=bool)
+grid2 = np.zeros((1000, 1000), 'int32')
+
+pattern = re.compile(r'(toggle|turn on|turn off)\s(\d+,\d+) through (\d+,\d+)')
+
+def part1(puzzle_in: str):
+	global grid
+	for line in puzzle_in.splitlines():
+		m = re.match(pattern, line)
+		action = m.group(1)
+		x0, y0 = list(map(int, m.group(2).split(',')))
+		x1, y1 = list(map(int, m.group(3).split(',')))
+		
+		if   action == 'turn on': grid[x0:x1+1, y0:y1+1] = True
+		elif action == 'turn off': grid[x0:x1+1, y0:y1+1] = False
+		elif action == 'toggle': grid[x0:x1+1, y0:y1+1] = ~grid[x0:x1+1, y0:y1+1]
+	return np.count_nonzero(grid)
+
+def part2(puzzle_in: str):
+	global grid2
+
+	for line in puzzle_in.splitlines():
+		m = re.match(pattern, line)
+		action = m.group(1)
+		x0, y0 = list(map(int, m.group(2).split(',')))
+		x1, y1 = list(map(int, m.group(3).split(',')))
+		
+		if   action == 'turn on': grid2[x0:x1+1, y0:y1+1] += 1
+		elif action == 'turn off':
+			grid2[x0:x1+1, y0:y1+1] -= 1
+			grid2[grid2 < 0] = 0
+		elif action == 'toggle': grid2[x0:x1+1, y0:y1+1] += 2
+		
+	return np.sum(grid2)
+
+def faster_part1(puzzle_in: str):
+	raise 'not implemented yet'
+
+def faster_part2(puzzle_in: str):
+	raise 'not implemented yet'
