30e4bf2c9ae9cd9c446b17197f5d4c7a20b7642b
EDDxample <EDDxample@gmail.com>
2021-04-25 21:52:47 +0200

    complete 2015/10

---
+++ aoc/2015/10/solution.py
@@ -0,0 +1,25 @@
+import re
+
+def part1(puzzle_in: str):
+	for i in range(40):
+		new_str = ''
+		m = re.findall(r'([0-9])(\1*)', puzzle_in)
+		for key, count in m:
+			new_str += str(len(count) + 1) + key
+		puzzle_in = new_str
+	return len(puzzle_in)
+
+def part2(puzzle_in: str):
+	for i in range(50):
+		new_str = ''
+		m = re.findall(r'([0-9])(\1*)', puzzle_in)
+		for key, count in m:
+			new_str += str(len(count) + 1) + key
+		puzzle_in = new_str
+	return len(puzzle_in)
+
+def faster_part1(puzzle_in: str):
+	raise 'not implemented yet'
+
+def faster_part2(puzzle_in: str):
+	raise 'not implemented yet'
