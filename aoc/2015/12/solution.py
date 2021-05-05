9b2ba9feb4dc79b00232dceb22f2b00bc525190f
EDDxample <EDDxample@gmail.com>
2021-05-05 02:26:09 +0200

    complete 2015/12

--- aoc/2015/12/solution.py
+++ aoc/2015/12/solution.py
@@ -2,12 +2,29 @@ import re, json
 
 num_pattern = re.compile(r'(-?\d+)')
 
+def count_values(obj):
+	count = 0
+	try: count = int(obj)
+	except TypeError:
+		if type(obj) is list:
+			for x in obj:
+				count += count_values(x)
+
+		elif type(obj) is dict:
+			if 'red' in obj.values(): return 0
+			for key in obj:
+				count += count_values(key)
+				count += count_values(obj[key])
+	except ValueError: pass
+	finally: return count
+
 def part1(puzzle_in: str):
 	return sum(map(int, re.findall(num_pattern, puzzle_in)))
 
 def part2(puzzle_in: str):
 	data: object = json.loads(puzzle_in)
-	
+	return count_values(data)
+
 
 def faster_part1(puzzle_in: str):
 	raise 'not implemented yet'
