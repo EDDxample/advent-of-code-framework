05bfd217fb5fcc9b34aadd5677801e63e5131748
E <a@b.c>
2021-05-09 17:56:25 +0200

    add get_description() and generate 2015/14

--- aoc.py
+++ aoc.py
@@ -1,4 +1,5 @@
-import requests, os, errno, sys, importlib, time
+import requests, os, errno, sys, importlib, time, re
+from markdownify import markdownify
 
 def main():
     try:
@@ -40,15 +41,20 @@ def run_solutions(year, day):
 
 def gen_day(year, day):
     path = f'aoc/{year}/{int(day):02}'
-    puzzle_input = get_input(year, day)
-    write_file(path+'/input.txt', puzzle_input)
-    write_file(path+'/description.txt', f'https://adventofcode.com/{year}/day/{day}')
+    write_file(path+'/input.txt', get_input(year, day))
+    write_file(path+'/description.md', get_description(year, day))
     write_file(path+'/solution.py', '\ndef part1(puzzle_in: str):\n\treturn \'not implemented yet\'\n\ndef part2(puzzle_in: str):\n\treturn \'not implemented yet\'\n\ndef faster_part1(puzzle_in: str):\n\traise \'not implemented yet\'\n\ndef faster_part2(puzzle_in: str):\n\traise \'not implemented yet\'\n')
 
 def get_input(year, day):
     with open('.token') as token:
         return requests.get(f'https://adventofcode.com/{year}/day/{day}/input', cookies={ 'session': token.read() }).text
-
+def get_description(year, day):
+    with open('.token') as token:
+        desc_pattern = re.compile(r'<article class="day-desc">[<>:!,.()?"*\w\-\+\\\s\=\/\n]*<\/article>')
+        page = requests.get(f'https://adventofcode.com/{year}/day/{day}', cookies={ 'session': token.read() }).text
+        
+        puzzles = re.findall(desc_pattern, page)
+        return '\n'.join(map(lambda s: markdownify(s, heading_style="ATX"), puzzles))
 
 
 # utils
---
+++ aoc/2015/14/solution.py
@@ -0,0 +1,12 @@
+
+def part1(puzzle_in: str):
+	return 'not implemented yet'
+
+def part2(puzzle_in: str):
+	return 'not implemented yet'
+
+def faster_part1(puzzle_in: str):
+	raise 'not implemented yet'
+
+def faster_part2(puzzle_in: str):
+	raise 'not implemented yet'
