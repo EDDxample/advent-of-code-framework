a486810a7c24baea6760c14eece3084c91e07ca8
EDDxample <EDDxample@gmail.com>
2021-05-20 01:18:09 +0200

    complete 2015/19

---
+++ aoc/2015/19/solution.py
@@ -0,0 +1,91 @@
+from collections import defaultdict
+from random import shuffle
+import re
+
+pattern = re.compile(r'(\w+) => (\w+)')
+
+def replace_string(string, key, val, pos):
+	return string[:pos] + val + string[pos + len(key):]
+
+def get_pattern_locations(dna, pattern):
+	try:
+		pos = dna.index(pattern, 0)
+		while True:
+			yield pos
+			pos = dna.index(pattern, pos + 1)
+	except ValueError: pass
+
+def part1(puzzle_in: str):
+	flag = False
+	trans = defaultdict(list)
+	dna = ''
+	
+	molecules = set()
+	
+	# parse input ==================
+
+	for line in puzzle_in.splitlines():
+		if line == '':
+			flag = True
+
+		elif flag:
+			dna = line
+
+		else:
+			match = pattern.match(line)
+			trans[match[1]].append(match[2])
+
+	# apply transformations ===============
+
+	for key, vals in trans.items():
+		for pos in get_pattern_locations(dna, key):
+			for val in vals:
+				molecules.add(replace_string(dna, key, val, pos))
+		
+	return len(molecules)
+
+def part2(puzzle_in: str):
+	flag = False
+	trans = defaultdict(list)
+	dna = ''
+	
+	molecules = set()
+	
+	# parse input ==================
+
+	for line in puzzle_in.splitlines():
+		if line == '':
+			flag = True
+
+		elif flag:
+			dna = line
+
+		else:
+			match = pattern.match(line)
+			trans[match[2]].append(match[1])
+
+	# solution ===============
+	
+	target = dna
+	steps = 0
+
+	while target != 'e':
+		tmp = target
+		for key, vals in trans.items():
+			if key not in target: continue
+
+			for val in vals:
+				target = target.replace(key, val, 1)
+				steps += 1
+		if tmp == target:
+			target = dna
+			steps = 0
+			shuffle(dna)
+	return steps
+
+
+def faster_part1(puzzle_in: str):
+	raise 'not implemented yet'
+
+def faster_part2(puzzle_in: str):
+	raise 'not implemented yet'
