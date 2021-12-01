610ff9b58b53e447050b1b2ad0a44e65391676c8
EDDxample <EDDxample@gmail.com>
2021-12-01 22:43:49 +0100

    2018 day 01

---
+++ aoc/2018/01/solution.py
@@ -0,0 +1,19 @@
+def part1(puzzle_in: str):
+	return sum(map(int, puzzle_in.splitlines()))
+
+def part2(puzzle_in: str):
+	i = 0
+	values = { 0:0 }
+	while True:
+		for n in map(int, puzzle_in.splitlines()):
+			i += n
+			if i in values:
+				return i
+			values[i] = 0
+
+
+def faster_part1(puzzle_in: str):
+	raise 'not implemented yet'
+
+def faster_part2(puzzle_in: str):
+	raise 'not implemented yet'
